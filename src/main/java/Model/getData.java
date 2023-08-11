1509cbae909c38889f5ff611400e88881df02539
phamkhang <trongkhang17102001@gmail.com>
2023-08-12 06:18:53 +0700

    add bill

--- src/main/java/Model/getData.java
+++ src/main/java/Model/getData.java
@@ -1,7 +1,13 @@
 package Model;
 
+import javafx.scene.Scene;
+
 public class getData {
     public static String user;
     public static String ma;
     public static String path;
+    public static Medicine medicine;
+    public static String state;
+    public static Scene current;
+    public static Bill bill;
 }
